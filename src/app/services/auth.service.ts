f28d42be3f4226dc62f5c4009cc3ac2262d30901
dudu9999 <ecaetanocorrea@gmail.com>
2020-09-10 17:55:24 -0300

    create interface e service product

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,19 +8,19 @@ import { User } from '../interfaces/user';
 export class AuthService {
   constructor(private afa: AngularFireAuth) { }
 
-    login(user: User) {
-      return this.afa.signInWithEmailAndPassword(user.email, user.password);
-    }
-
-    register(user: User) {
-      return this.afa.createUserWithEmailAndPassword(user.email, user.password);
-    }
-
-    logout() {
-      return this.afa.signOut();
-    }
-
-    getAuth() {
-      return this.afa;
-    }
+  login(user: User) {
+    return this.afa.signInWithEmailAndPassword(user.email, user.password);
   }
+
+  register(user: User) {
+    return this.afa.createUserWithEmailAndPassword(user.email, user.password);
+  }
+
+  logout() {
+    return this.afa.signOut();
+  }
+
+  getAuth() {
+    return this.afa;
+  }
+}
